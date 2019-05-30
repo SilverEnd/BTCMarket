4648cf6039982d8600461bb8e5f30dcab88c8e38
nyzo1 <yzuo@woolworths.com.au>
2019-05-30 17:37:13 +1000

    Setting up modules

--- app/src/main/java/com/erikzuo/btcmarkets/MainActivity.kt
+++ app/src/main/java/com/erikzuo/btcmarkets/MainActivity.kt
@@ -6,12 +6,21 @@ package com.erikzuo.btcmarkets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.erikzuo.btcmarkets.R
+import com.erikzuo.btcmarkets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var layoutBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        layoutBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        layoutBinding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainActivity_to_marketFragment)
+        }
     }
 }
